6ea21685e2e563c8dbf49b6ab6372e2eaa888193
Elijah Perrault <elijah@starcoder.info>
2011-04-28 11:29:22 -0600

    Calc 1.01: New and improved Calc. Now uses Furl.

--- modules/Calc.pm
+++ modules/Calc.pm
@@ -6,25 +6,23 @@ use strict;
 use warnings;
 use API::Std qw(cmd_add cmd_del trans);
 use API::IRC qw(privmsg notice);
-use LWP::UserAgent;
+use Furl;
 use URI::Escape;
 use JSON -support_by_pp;
 
 # Initialization subroutine.
-sub _init 
-{
+sub _init {
     # Create the CALC command.
-    cmd_add("CALC", 0, 0, \%M::Calc::HELP_CALC, \&M::Calc::calc) or return;
+    cmd_add('CALC', 0, 0, \%M::Calc::HELP_CALC, \&M::Calc::cmd_calc) or return;
 
     # Success.
     return 1;
 }
 
 # Void subroutine.
-sub _void 
-{
+sub _void {
     # Delete the CALC command.
-    cmd_del("CALC") or return;
+    cmd_del('CALC') or return;
 
     # Success.
     return 1;
@@ -37,90 +35,105 @@ our %FHELP_CALC = (
 );
 
 # Callback for CALC command.
-sub calc
-{
-    my ($src, @args) = @_;
+sub cmd_calc {
+    my ($src, @argv) = @_;
 
-    # Create an instance of LWP::UserAgent.
-    my $ua = LWP::UserAgent->new();
-    $ua->agent('Auto IRC Bot');
-    $ua->timeout(2);
+    # Create an instance of Furl.
+    my $ua = Furl->new(
+        agent => 'Auto IRC Bot',
+        timeout => 5,
+    );
+
     # Create an instance of JSON.
     my $json = JSON->new();    
+    
     # Put together the call to the Google Calculator API. 
-    if (!defined $args[0]) {
-        notice($src->{svr}, $src->{nick}, trans("Not enough parameters").".");
+    if (!defined $argv[0]) {
+        notice($src->{svr}, $src->{nick}, trans('Not enough parameters').q{.});
         return;
     }
-    my $expr = join(' ', @args);
-    my $url = "http://www.google.com/ig/calculator?q=".uri_escape($expr);
+    my $expr = join ' ', @argv;
+    my $url = 'http://www.google.com/ig/calculator?q='.uri_escape($expr);
     # Get the response via HTTP.
     my $response = $ua->get($url);
 
     if ($response->is_success) {
-        # If successful, decode the content.
-        my $d = $json->allow_nonref->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($response->decoded_content);
+        # If successful, get the content.
+        my $data = $json->allow_nonref->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($response->content);
 
-        if ($d->{error} eq "" or $d->{error} == 0) {
+        if ($data->{error} eq q{} or $data->{error} eq 0) {
             # And send to channel
-            privmsg($src->{svr}, $src->{chan}, "Result: ".$d->{lhs}." = ".$d->{rhs});
+            privmsg($src->{svr}, $src->{chan}, "Result: $data->{lhs} = $data->{rhs}");
         }
         else {
             # Otherwise, send an error message.
-            privmsg($src->{svr}, $src->{chan}, "Google Calculator sent an error.");
+            privmsg($src->{svr}, $src->{chan}, 'Google Calculator sent an error.');
         }
     }
     else {
         # Otherwise, send an error message.
-        privmsg($src->{svr}, $src->{chan}, "An error occurred while sending your expression to Google Calculator.");
+        privmsg($src->{svr}, $src->{chan}, 'An error occurred while sending your expression to Google Calculator.');
     }
 
     return 1;
 }
 
 # Start initialization.
-API::Std::mod_init('Calc', 'Xelhua', '1.00', '3.0.0a10');
-# build: cpan=LWP::UserAgent,URI::Escape,JSON,JSON::PP perl=5.010000
+API::Std::mod_init('Calc', 'Xelhua', '1.01', '3.0.0a11');
+# build: cpan=Furl,URI::Escape,JSON,JSON::PP perl=5.010000
 
 __END__
 
-=head1 Calc
+=head1 NAME
 
-=head2 Description
+Calc - Interface to Google Calculator
 
-=over
+=head1 VERSION
 
-This module adds the CALC command for evaluating an expression using 
-Google Calculator.
+ 1.01
 
-=back
+=head1 SYNOPSIS
 
-=head2 Examples
 
-=over
 
-<JohnSmith> !calc 1+1
-<Auto> Result: 1+1 = 2
 
-=back
+=head1 DESCRIPTION
 
-=head2 To Do
+This module creates the CALC command for evaluating expressions on Google
+Calculator and returning the result.
 
-=over
+=head1 DEPENDENCIES
 
-* Add Spanish and French translations for the help hash.
-
-=back
-
-=head2 Technical
+This module depends on the following CPAN modules:
 
 =over
 
-This module requires LWP::UserAgent, URI::Escape and JSON/JSON::PP. 
-All are obtainable from the CPAN <http://www.cpan.org>.
+=item L<Furl>
 
-This module is compatible with Auto version 3.0.0a10+.
+This is the HTTP agent this module uses.
+
+=item L<URI::Escape>
+
+This is used to escape special characters from expressions.
+
+=item L<JSON>
+
+This is used to parse the data returned by the Google Calculator API.
 
 =back
+
+=head1 AUTHOR
+
+This module was written by Matthew Barksdale.
+
+This module is maintained by Xelhua Development Group.
+
+=head1 LICENSE AND COPYRIGHT
+
+This module is Copyright 2010-2011 Xelhua Development Group.
+
+Released under the same licensing terms as Auto itself.
+
+=cut
 
 # vim: set ai et sw=4 ts=4:
