8283e8ec480a98c39fa2fbeec2f3cb83910d570b
Matthew Barksdale <mattwb65@gmail.com>
2011-05-14 16:35:04 -0400

    Added a GoMini module to shorten a URL with gomini.me

---
+++ modules/GoMini.pm
@@ -0,0 +1,117 @@
+# Module: GoMini. See below for documentation.
+# Copyright (C) 2010-2011 Xelhua Development Group, et al.
+# This program is free software; rights to this code are stated in doc/LICENSE.
+package M::GoMini;
+use strict;
+use warnings;
+use API::Std qw(cmd_add cmd_del trans);
+use API::IRC qw(privmsg notice);
+use Furl;
+
+# Initialization subroutine.
+sub _init {
+    # Create the GOMINI command.
+    cmd_add('GOMINI', 0, 0, \%M::GoMini::HELP_GOMINI, \&M::GoMini::cmd_gomini) or return;
+
+    # Success.
+    return 1;
+}
+
+# Void subroutine.
+sub _void {
+    # Delete the GOMINI command.
+    cmd_del('GOMINI') or return;
+
+    # Success.
+    return 1;
+}
+
+# Help hash.
+our %HELP_GOMINI = (
+    en => "This command will return a shortend version of the URL specified using gomini.me \2Syntax:\2 GOMINI <long url>",
+);
+
+# Callback for GOMINI command.
+sub cmd_gomini {
+    my ($src, @args) = @_;
+    notice($src->{svr}, $src->{nick}, "Invalid parameters. Syntax: GOMINI <url>") if !$args[0];
+    # Create an instance of Furl.
+    my $ua = Furl->new(
+        agent => 'Auto IRC Bot',
+        timeout => 5,
+    );
+
+    # Get the GoMini shortend URL.
+    my $rp = $ua->post("http://gomini.me/api/shorten", [], [ url => $args[0], ]);
+
+    if ($rp->is_success) {
+        # If successful, get the content.
+
+        my $url = $rp->content;
+        
+        # If there's an error return that.
+        privmsg($src->{svr}, $src->{chan}, "\2gomini.me:\2 Format Error: Check to make sure you include a valid http prefix (eg. http://).") and return if $url eq 'FMT_ERROR';
+        privmsg($src->{svr}, $src->{chan}, "\2gomini.me:\2 Database Error: GoMini encountered a database error.") and return if $url eq 'DB_ERROR';
+        # Return the shortend URL.
+        privmsg($src->{svr}, $src->{chan}, "\2gomini.me:\2 $args[0] -> $url");
+
+    }
+    else {
+        # Otherwise, send an error message.
+        privmsg($src->{svr}, $src->{chan}, 'An error occurred while creating the URL.');
+    }
+
+    return 1;
+}
+
+# Start initialization.
+API::Std::mod_init('GoMini', 'Xelhua', '1.00', '3.0.0a11');
+# build: cpan=Furl perl=5.010000
+
+__END__
+
+=head1 NAME
+
+ GoMini - A module for making a gomini.me URL.
+
+=head1 VERSION
+
+ 1.00
+
+=head1 SYNOPSIS
+
+ <starcoder> !gomini http://google.com
+ <blue> gomini.me: http://google.com -> http://gomini.me/T
+
+=head1 DESCRIPTION
+
+This module creates the GOMINI command, which will make a shortened URL
+of a specified long URL using gomini.me
+
+=head1 DEPENDENCIES
+
+This module depends on the following CPAN modules:
+
+=over
+
+=item L<Furl>
+
+This is the HTTP agent used by this module.
+
+=back
+
+=head1 AUTHOR
+
+This module was written by Matthew Barksdale.
+
+This module is maintained by Xelhua Development Group.
+
+=head1 LICENSE AND COPYRIGHT
+
+This module is Copyright 2010-2011 Xelhua Development Group.
+
+Released under the same licensing terms as Auto itself.
+
+=cut
+
+# vim: set ai et sw=4 ts=4:
