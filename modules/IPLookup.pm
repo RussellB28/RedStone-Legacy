042b7202ac42348983583d3bc671882e5114b09e
Matthew <matt@mattwb65.net>
2011-11-11 07:32:42 +0100

    IPLookup 1.01: Changed to a different provider module, added more infomration, and added IP validation

--- modules/IPLookup.pm
+++ modules/IPLookup.pm
@@ -7,12 +7,14 @@ use strict;
 use warnings;
 use API::Std qw(cmd_add cmd_del conf_get);
 use API::IRC qw(privmsg);
-use IP::Info;
+use Net::Whois::IANA;
+use Regexp::Common qw/net/;
+use Socket;
 
 # Initalization subroutine.
 sub _init {
     # Create IP lookup command
-    cmd_add('IPLOOKUP',0,0, \%M::IPLookup::HELP_IPLOOKUP, \&M::IPLookup::cmd_iplookup) or return;
+    cmd_add('IPLOOKUP', 0, 0, \%M::IPLookup::HELP_IPLOOKUP, \&M::IPLookup::cmd_iplookup) or return;
     
     # Success
     return 1;
@@ -33,22 +35,27 @@ sub cmd_iplookup {
     my ($src, @args) = @_;
     my $ip = join(' ', @args);
     
+    privmsg($src->{svr}, $src->{chan}, "The IP address, ".$ip.", is not valid.") and return if $ip !~ /^$RE{net}{IPv4}$/;
+
     if ($ip) {
-        eval {
-            my $api_key = (conf_get("ipinfo:api_key"))[0][0] if conf_get("ipinfo:api_key");
-            my $api_secret = (conf_get("ipinfo:api_secret"))[0][0] if conf_get("ipinfo:api_secret");
-            my $ip_info = IP::Info->new($api_key,$api_secret);
-    
-            my $ip_response = $ip_info->ipaddress($ip);
-    
-            privmsg($src->{svr}, $src->{chan}, "IP: ".$ip." | ASN: ".$ip_response->asn()." | Carrier: ".ucfirst($ip_response->carrier())." | Organization: ".ucfirst($ip_response->organization())." | Location: ".ucfirst($ip_response->city()).", ".ucfirst($ip_response->country()));
-        };
-        
-        if ($@) {
-            privmsg($src->{svr}, $src->{chan}, 'An error occured while looking up the IP address.'); 
-            
+        my $iana = Net::Whois::IANA->new;
+        $iana->whois_query(-ip => $ip);  
+  
+        my $info = $iana->fullinfo;
+        $info =~ s/:\s+/:/g;
+        my ($asn, $city, $state);
+        if ($info =~ /OriginAS:AS(.*)/) {
+            $asn = $1;
         }
-    
+        if ($info =~ /City:(.*)/) {
+            $city = $1;
+        }
+        if ($info =~ /StateProv:(.*)/) {
+            $state = $1;
+        }
+        my $iaddr = inet_aton($ip);
+        my $rdns = gethostbyaddr($iaddr, AF_INET);
+        privmsg($src->{svr}, $src->{chan}, "IP: ".$ip." | ASN: ".(defined($asn) ? $asn : 'N/A')." | Location: ".(defined($city) ? $city : 'N/A').", ".(defined($state) ? $state : 'N/A').", ".$iana->country." | Netname: ".$iana->netname." | Description: ".$iana->descr." | Source: ".$iana->source." | rDNS: ".$rdns." | Abuse: ".$iana->abuse);
     } else {
         privmsg($src->{svr}, $src->{chan}, 'Not enough parameters.');
     }
@@ -57,8 +64,8 @@ sub cmd_iplookup {
 }
 
 # Start initialization.
-API::Std::mod_init('IPLookup', 'Xelhua', '1.00', '3.0.0a11');
-# build: cpan=IP::Info,Readonly::XS perl=5.010000
+API::Std::mod_init('IPLookup', 'Xelhua', '1.01', '3.0.0a11');
+# build: cpan=Net::Whois::IANA,Regexp::Common perl=5.010000
 
 
 __END__
@@ -67,25 +74,17 @@ __END__
 IP Lookup  - Lookup information on an IP address
 
 =head1 VERSION
-1.00
+1.01
 
 =head1 SYNOPSIS
 <JohnSmith> !iplookup 8.8.8.8
-<Auto> IP: 8.8.8.8 | ASN: 15169 | Carrier: Google inc. | Organization: Google incorporated | Location: Mountain view, United states
+<Auto> IP: 8.8.8.8 | ASN: N/A | Country: Mountain View, CA, US | Netname: LVLT-ORG-8-8 | Description: Level 3 Communications, Inc. | Source: ARIN | rDNS google-public-dns-a.google.com | Abuse: security@level3.com
 
 =head1 DESCRIPTION
 This module adds the IP LOOKUP command for retrieving ASN info for an IP.
 
 =head1 INSTALL
-This module requires IP::Info and Readonly::XS. Both are obtainable from CPAN <http://www.cpan.org>.
-
-Before using IPLookup,add the following block in your configuration file:
-ipinfo {
-    api_key "API Key";
-    api_secret "API Secret";
-}
-
-You can get API credentials by registering at http://developer.quova.com/
+This module requires Net::Whois::IANA and Regexp::Common. Both are obtainable from CPAN <http://www.cpan.org>.
 
 =head1 AUTHOR
 This module was written by Liam Smith <me@liam.co>.
