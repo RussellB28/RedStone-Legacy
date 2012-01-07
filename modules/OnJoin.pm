14bf4babaa9e551368c339dfa8513519675c49f8
Matthew <matt@mattwb65.net>
2012-01-07 08:17:15 +0100

    Added a module similar to HelloChan but configurable. This module is more intended to replace HelloChan because without defining the configuration value it will produce the same behavior as HelloChan. Eventual plans for this module include making it configurable per channel.

--- modules/OnJoin.pm
+++ modules/OnJoin.pm
@@ -1,46 +1,48 @@
-# Module: HelloChan.
+# Module: OnJoin.
 # Copyright (C) 2010-2012 Xelhua Development Group, et al.
 # This program is free software; rights to this code are stated in doc/LICENSE.
-package M::HelloChan;
+package M::OnJoin;
 use strict;
 use warnings;
-use API::Std qw(hook_add hook_del);
+use API::Std qw(hook_add hook_del conf_get);
 use API::IRC qw(privmsg);
+our $greet;
 
 # Initialization subroutine.
 sub _init {
     # Add a hook for when we join a channel.
-    hook_add('on_ucjoin', 'HelloChan', \&M::HelloChan::hello) or return;
+    hook_add('on_ucjoin', 'OnJoin', \&M::OnJoin::hello) or return;
+    $greet = (conf_get('onjoin_greet') ? (conf_get('onjoin_greet'))[0][0] : "Hello channel! I am a bot!");
     return 1;
 }
 
 # Void subroutine.
 sub _void {
-    # Delete the hook.
-    hook_del('on_ucjoin', 'HelloChan') or return;
+    # Delete the hooks.
+    hook_del('on_ucjoin', 'OnJoin') or return;
     return 1;
 }
 
 # Main subroutine.
 sub hello {
-    my (($svr, $chan)) = @_;
+    my ($svr, $chan) = @_;
     
     # Send a PRIVMSG.
-    privmsg($svr, $chan, 'Hello world! I am a bot!');
+    privmsg($svr, $chan, $greet);
     
     return 1;
 }
 
 
 # Start initialization.
-API::Std::mod_init('HelloChan', 'Xelhua', '1.00', '3.0.0a11');
+API::Std::mod_init('OnJoin', 'Xelhua', '1.00', '3.0.0a11');
 # build: perl=5.010000
 
 __END__
 
 =head1 NAME
 
-HelloChan - An example module. Also, cows go moo.
+OnJoin - An example module. Also, cows go moo.
 
 =head1 VERSION
 
@@ -53,16 +55,17 @@ HelloChan - An example module. Also, cows go moo.
 
 =head1 DESCRIPTION
 
-This module sends "Hello channel! I am a bot!" whenever it 
-joins a channel.
+This module sends "Hello channel! I am a bot!" or a custom 
+greeting whenever it joins a channel.
 
 =head1 INSTALL
 
-No additonal steps need to be taking to use this module.
+Add onjoin_greet to your configuration file.
+Example: onjoin_greet "Hello channel! I am not a bot!";
 
 =head1 AUTHOR
 
-This module was written by Elijah Perrault.
+This module was written by Matthew Barksdale.
 
 This module is maintained by Xelhua Development Group.
 
