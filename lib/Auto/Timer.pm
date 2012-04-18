bcfa400422d329757bb21271426bafa2ec828e74
Matthew Barksdale <matt@mattwb65.com>
2012-04-18 04:58:20 +0200

    Added Auto::Timer. Will be used very soon with the switch to IO::Async

---
+++ lib/Auto/Timer.pm
@@ -0,0 +1,52 @@
+# Copyright (c) 2012 Ethrik Development Group
+# see doc/LICENSE for license information.
+package Auto::Timer;
+ 
+use warnings;
+use strict;
+use base 'IO::Async::Timer::Periodic';
+use feature qw(say);
+
+sub new {
+    my ($class, %opts) = @_;
+
+    # Make sure all requirements are present.
+    foreach my $what (qw|name delay function|) {
+        next if exists $opts{$what};
+        $opts{name} ||= 'unknown';
+        $class->dbug("Timer $opts{name} does not have a '$what' option.");
+        return;
+    }
+
+    # Create the actual timer.
+    my $timer = $class->SUPER::new(
+        interval => $opts{delay},
+        on_tick  => sub {
+            my $self = shift;
+            $self->{function}->();
+            if (!$self->{repeat}) {
+                $self->stop;
+                delete $Auto::TIMERS{$opts{name}};
+            }
+        }
+    ) or $class->dbug("Timer $opts{name} creation failed.") and return;
+
+    $class->dbug("Timer $opts{name} created.");
+
+    $timer->{$_} = $opts{$_} foreach qw(name function repeat);
+    return $timer;
+}
+
+sub go {
+    my $timer = shift;
+    $timer->start;
+    $Auto::loop->add($timer);
+    $timer->dbug("Timer $$timer{name} started.");
+    return $timer;
+}
+
+sub dbug { shift; say shift if $Auto::DEBUG; }
+
+1;
+
+# vim: set ai et sw=4 ts=4:
