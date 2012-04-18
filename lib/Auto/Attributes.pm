b9523c8374d1b6679627c1d395b545e9538a0acd
Matthew Barksdale <matt@mattwb65.com>
2012-04-19 00:00:17 +0200

    Updated Attributes for 5.10 compatibility

--- lib/Auto/Attributes.pm
+++ lib/Auto/Attributes.pm
@@ -1,6 +1,6 @@
 # Copyright (c) 2012 Ethrik Development Group
 # see doc/LICENSE for license information.
-package Attributes;
+package Auto::Attributes;
 
 use warnings;
 use strict;
@@ -12,39 +12,46 @@ sub import {
     foreach my $sym (@_[1..$#_]) {
         my $name = $sym;
 
-        # this is quite ugly, but it's the only way to do it for compatibility with perl 5.12
-        my $code = do { given (substr $name, 0, 1) {
+        # this is quite ugly, but it's the only way to do it for compatibility with perl 5.10
+        my $symb = substr $name, 0, 1;
+        my $code;
 
-            # arrays
-            when ('@') { $name = substr $name, 1; sub {
-                my $self = shift;
-                if ($self->{$name} && ref $self->{$name} eq 'ARRAY') {
-                    return @{$self->{$name}};
+        given ($sym) {
+            when (['@', '%']) {
+                $name = substr $name, 1;
+                continue;
+            }
+
+            # array
+            when ('@') {
+                $code = sub {
+                    my $self = shift;
+                    if ($self->{$name} && ref $self->{$name} eq 'ARRAY') {
+                        return @{$self->{$name}};
+                    }
+                    # empty array
+                    return @{$self->{$name} = []};
                 }
-                # empty array
-                my @a;
-                $self->{$name} = \@a;
-                @a;
-            } }
+            }
 
-            # hashes
-            when ('%') { $name = substr $name, 1; sub {
-                my $self = shift;
-                if ($self->{$name} && ref $self->{$name} eq 'HASH') {
-                    return %{$self->{$name}};
+            # hash
+            when ('%') {
+                $code = sub {
+                    my $self = shift;
+                    if ($self->{$name} && ref $self->{$name} eq 'HASH') {
+                        return %{$self->{$name}};
+                    }
+                    # empty hash
+                    return %{$self->{$name} = {}};
                 }
-                # empty hash
-                my %h;
-                $self->{$name} = \%h;
-                %h;
-            } }
+            }
 
-            # plain old value
-            default { sub {
-                shift->{$name};
-            } }
+            # scalar
+            default {
+                $code = sub { shift->{$name} }
+            }
+        }
 
-        } };
         *{$package.q(::).$name} = $code;
     }
 }
