d409a1a47378d7e73ee0d3f995a6c4d949b9fa30
Russell M Bradford <russell@rbradford.me>
2013-09-21 21:07:13 +0100

    Re-written the IPLookup Module - This now supports IPv4 and IPv6 and takes domain names as an argument

--- modules/IPLookup.pm
+++ modules/IPLookup.pm
@@ -1,5 +1,5 @@
 # Module: IP Lookup. See below for documentation. 
-# Copyright (C) 2010-2012 Xelhua Development Group, et al.
+# Copyright (C) 2013 Russell M Bradford, et al.
 # This program is free software; rights to this code are stated in doc/LICENSE.
 package M::IPLookup;
 
@@ -7,14 +7,16 @@ use strict;
 use warnings;
 use API::Std qw(cmd_add cmd_del conf_get);
 use API::IRC qw(privmsg);
-use Net::Whois::IANA;
 use Regexp::Common qw/net/;
+use JSON;
 use Socket;
+use Net::DNS;
 
 # Initalization subroutine.
 sub _init {
     # Create IP lookup command
     cmd_add('IPLOOKUP', 0, 0, \%M::IPLookup::HELP_IPLOOKUP, \&M::IPLookup::cmd_iplookup) or return;
+    cmd_add('IL', 0, 0, \%M::IPLookup::HELP_IPLOOKUP, \&M::IPLookup::cmd_iplookup) or return;
     
     # Success
     return 1;
@@ -22,6 +24,7 @@ sub _init {
 
 sub _void  {
     cmd_del('IPLOOKUP') or return;
+    cmd_del('IL') or return;
     
     return 1;
 }
@@ -32,39 +35,100 @@ our %HELP_IPLOOKUP = (
 );
 
 sub cmd_iplookup {
-    my ($src, @args) = @_;
-    my $ip = join(' ', @args);
-    
-    privmsg($src->{svr}, $src->{chan}, 'Not enough parameters.') and return if !@args;
-    privmsg($src->{svr}, $src->{chan}, "The IP address, ".$ip.", is not valid.") and return if $ip !~ /^$RE{net}{IPv4}$/;
+    my ($src, @argv) = @_;
 
-    if ($ip) {
-        my $iana = Net::Whois::IANA->new;
-        $iana->whois_query(-ip => $ip);  
-  
-        my $info = $iana->fullinfo;
-        $info =~ s/:\s+/:/g;
-        my ($asn, $city, $state);
-        if ($info =~ /OriginAS:AS(.*)/) {
-            $asn = $1;
-        }
-        if ($info =~ /City:(.*)/) {
-            $city = $1;
-        }
-        if ($info =~ /StateProv:(.*)/) {
-            $state = $1;
-        }
-        my $iaddr = inet_aton($ip);
-        my $rdns = gethostbyaddr($iaddr, AF_INET);
-        privmsg($src->{svr}, $src->{chan}, "IP: ".$ip." | ASN: ".(defined($asn) ? $asn : 'N/A')." | Location: ".(defined($city) ? $city : 'N/A').", ".(defined($state) ? $state : 'N/A').", ".$iana->country." | Netname: ".$iana->netname." | Description: ".$iana->descr." | Source: ".$iana->source." | rDNS: ".(defined($rdns) ? $rdns : 'None')." | Abuse: ".$iana->abuse);
+    if(!defined($argv[0]))
+    {
+        privmsg($src->{svr}, $src->{chan}, "Not Enough Parameters. Please specify a Domain or IPv4, IPv6 address");        
+        return 0;
     }
-    
+
+    my $api_url = "http://ip-api.com/json/".$argv[0];
+
+
+    $Auto::http->request(
+        url => $api_url,
+        on_response => sub {
+            my $result = shift;
+
+            if($result->is_success)
+            {
+                my $jsonresp = $result->decoded_content;
+                my $jsonbits = decode_json($jsonresp);
+                if($jsonbits->{"status"} eq "fail")
+                {
+                    privmsg($src->{svr}, $src->{chan}, "An Error Occured: ".$jsonbits->{"message"}." - Did you check that the IP was valid??");
+                    return 0;
+                }
+
+                my $hexip;
+                foreach (split /\./, $argv[0]) 
+                { 
+	                $hexip .= sprintf("\U%02x", $_); 
+                }
+                my @octets;
+                my $longip;
+                @octets = split(/\./, $argv[0]);
+                $longip = ($octets[0]*1<<24)+($octets[1]*1<<16)+($octets[2]*1<<8)+($octets[3]);
+
+                my $rdns;
+                my $res = Net::DNS::Resolver->new;
+                $res->tcp_timeout(3);
+                $res->udp_timeout(3);
+                my $dnsq = $res->search($argv[0]);
+                if ($dnsq) {
+                    foreach my $dnsr ($dnsq->answer) {
+                        next if $dnsr->type ne 'PTR';
+                        $rdns = $dnsr->rdatastr;
+                    }
+                }
+                else
+                {
+                    $rdns = "None";
+                }
+
+                if($jsonbits->{"city"} eq "") { $jsonbits->{"city"} = "N/A"; }
+                if($jsonbits->{"regionName"} eq "") { $jsonbits->{"regionName"} = "N/A"; }
+                if($jsonbits->{"countryCode"} eq "") { $jsonbits->{"countryCode"} = "N/A"; }
+                if($jsonbits->{"isp"} eq "") { $jsonbits->{"isp"} = "N/A"; }
+                if($jsonbits->{"org"} eq "") { $jsonbits->{"org"} = "N/A"; }
+                if($jsonbits->{"as"} eq "") { $jsonbits->{"as"} = "N/A"; }
+                
+
+                if($argv[0] !~ /^$RE{net}{IPv4}$/)
+                {
+                    privmsg($src->{svr}, $src->{target}, "\002Information for IP: ".$jsonbits->{"query"}."\002");
+                }
+                else
+                {
+                    privmsg($src->{svr}, $src->{target}, "\002Information for IP: ".$jsonbits->{"query"}." (Hex: 0x$hexip - Long: $longip)\002");
+                }
+                privmsg($src->{svr}, $src->{target}, "Reverse DNS: ".$rdns."");
+                privmsg($src->{svr}, $src->{target}, "Location: ".$jsonbits->{"city"}.", ".$jsonbits->{"regionName"}.", ".uc($jsonbits->{"countryCode"})."");
+                privmsg($src->{svr}, $src->{target}, "ISP: ".$jsonbits->{"isp"}."");
+                privmsg($src->{svr}, $src->{target}, "Organisation: ".$jsonbits->{"org"}."");
+                privmsg($src->{svr}, $src->{target}, "AS Information: ".$jsonbits->{"as"}."");
+                #privmsg($src->{svr}, $src->{target}, "Coordinates: ".$jsonbits->{"lat"}.", ".$jsonbits->{"lon"});
+                
+            }
+            else
+            {
+                privmsg($src->{svr}, $src->{chan}, "An unexpected error occured. Please try again shortly.");
+                return 0;
+            }
+        },
+        on_error => sub {
+                my $error = shift;
+                privmsg($src->{svr}, $src->{chan}, "An error occured: $error");
+                return 0;
+        }
+    );    
     return 1;
 }
 
 # Start initialization.
-API::Std::mod_init('IPLookup', 'Xelhua', '1.01', '3.0.0a11');
-# build: cpan=Net::Whois::IANA,Regexp::Common perl=5.010000
+API::Std::mod_init('IPLookup', 'Russell M Bradford', '2.00', '3.0.0a11');
+# build: cpan=Net::DNS,Regexp::Common,JSON perl=5.010000
 
 
 __END__
@@ -73,25 +137,30 @@ __END__
 IP Lookup  - Lookup information on an IP address
 
 =head1 VERSION
-1.01
+2.00
 
 =head1 SYNOPSIS
-<JohnSmith> !iplookup 8.8.8.8
-<Auto> IP: 8.8.8.8 | ASN: N/A | Country: Mountain View, CA, US | Netname: LVLT-ORG-8-8 | Description: Level 3 Communications, Inc. | Source: ARIN | rDNS google-public-dns-a.google.com | Abuse: security@level3.com
+<Roy> !iplookup 2001:1af8:4300:a011:14:0:0:0
+<RedStone> Information for IP: 2001:1af8:4300:a011:14:0:0:0
+<RedStone> Reverse DNS: ub3r.l33t.c0de5.org.
+<RedStone> Location: N/A, N/A, NL
+<RedStone> ISP: N/A
+<RedStone> Organisation: N/A
+<RedStone> AS Information: AS16265 LeaseWeb B.V.
 
 =head1 DESCRIPTION
-This module adds the IP LOOKUP command for retrieving ASN info for an IP.
+This module adds the IP LOOKUP command for retrieving ASN info for an IPv4 or IPv6 address.
 
 =head1 INSTALL
-This module requires Net::Whois::IANA and Regexp::Common. Both are obtainable from CPAN <http://www.cpan.org>.
+This module requires Net::DNS, JSON and Regexp::Common. All are obtainable from CPAN <http://www.cpan.org>.
 
 =head1 AUTHOR
-This module was written by Liam Smith <me@liam.co>.
-This module is maintained by Liam Smith <me@liam.co>.
+This module was written by Russell M Bradford <russell[at]rbradford.me>.
+This module is maintained by Russell M Bradford <russell[at]rbradford.me>.
 
 =head1 LICENSE AND COPYRIGHT
-This module is Copyright 2010-2012 Xelhua Development Group, et al.
-This module is released under the same licensing terms as Auto itself.
+This module is Copyright 2013 Russell M Bradford, et al.
+This module is released under the same licensing terms as RedStone itself.
 =cut
 
 # vim: set ai et sw=4 ts=4:
