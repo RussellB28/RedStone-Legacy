59cc90e60a096a80d71754dc9d9b1d7e1bbc6b64
Russell Bradford <russell@rbradford.me>
2014-07-25 10:22:34 +0100

    New Version of UnixTime: Supports changing Date to UnixTime and UnixTime to Date formats

--- modules/UnixTime.pm
+++ modules/UnixTime.pm
@@ -1,11 +1,12 @@
 # Module: UnixTime. See below for documentation.
-# Copyright (C) 2012-2014 RedStone Development Group, et al.
+# Copyright (C) 2014 RedStone Development Group, et al.
 # This program is free software; rights to this code are stated in doc/LICENSE.
 package M::UnixTime;
 use strict;
 use warnings;
-use API::Std qw(cmd_add cmd_del);
+use API::Std qw(cmd_add cmd_del trans);
 use API::IRC qw(privmsg);
+use Date::Parse qw(str2time);
 
 # Initialization subroutine.
 sub _init {
@@ -16,7 +17,7 @@ sub _init {
 		# Success.
 		return 1;
 	} else {
-		cmd_add('UNIXTIME', 0, 0, \%M::UnixTime::HELP_UNIXTIME, \&M::UnixTime::cmd_unixtime2) or return;
+		return;
 	}
 }
 
@@ -31,8 +32,8 @@ sub _void {
 
 # Help hash for UNIXTIME.
 our %HELP_UNIXTIME = (
-    en => "This command changes a set of numbers to the appropriate normal time format. \2Syntax:\2 UNIXTIME <TIME>",
-	#nl => "Dit commando verandert een tijd van unixtime naar normaal leesbare tijd \2Syntax:\2 UNIXTIME <TIME>",
+    en => "This command changes a set of numbers to the appropriate normal time format. \2Syntax:\2 UNIXTIME <TO/FROM> <DATETIME/UNIXTIME>",
+	#nl => "Dit commando verandert een tijd van unixtime naar normaal leesbare tijd \2Syntax:\2 UNIXTIME <TO/FROM> <DATETIME/UNIXTIME>",
 );
 
 # Callback for UNIXTIME command.
@@ -42,47 +43,70 @@ sub cmd_unixtime {
 		privmsg($src->{svr}, $src->{chan}, trans('Too little parameters').q{.});
         return;
 	}
-	if(defined($argv[1])) {
-		privmsg($src->{svr}, $src->{chan}, trans('Too many parameters').q{.});
-        return;
-	}
-	if(Scalar::Util::looks_like_number($argv[0])) {
-		my $output;
-		my $timevariable;
-		$timevariable = "date -d @".$argv[0];
-		$output = `$timevariable`;
-		$output =~ s/\n//g;
-		privmsg($src->{svr}, $src->{chan}, "Unixtime format: \2$argv[0]\2 is \2$output\2");
-	} else {
-		privmsg($src->{svr}, $src->{chan}, "ERROR: Incorrect time format.");
-	}
-    return 1;
-}
 
-sub cmd_unixtime2 {
-	my ($src, @argv) = @_;
-	if(!defined($argv[0])) {
-		privmsg($src->{svr}, $src->{chan}, trans('Too little parameters').q{.});
-        return;
-	}
-	if(defined($argv[1])) {
-		privmsg($src->{svr}, $src->{chan}, trans('Too many parameters').q{.});
-        return;
-	}
-	if(Scalar::Util::looks_like_number($argv[0])) {
-		my $time = time;
-		my @months = ("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
-		my ($sec, $min, $hour, $day,$month,$year) = (localtime($time))[0,1,2,3,4,5];
-		privmsg($src->{svr}, $src->{chan}, "Unixtime format: \2$argv[0]\2 is \2".$months[$month]." ".$day.", ".($year+1900)." ".$hour.":".$min.":".$sec."\2");
-	} else {
-		privmsg($src->{svr}, $src->{chan}, "ERROR: Incorrect time format.");
-	}
+    if(lc($argv[0]) eq "from")
+    {
+
+	    if(lc($argv[1]) eq "now")
+	    {
+		    my $output;
+		    my $timevariable;
+		    $timevariable = "date -d @".time();
+		    $output = `$timevariable`;
+		    $output =~ s/\n//g;
+		    privmsg($src->{svr}, $src->{chan}, "Unixtime format: \2NOW\2 is \2".$output."\2");
+		    return;
+	    }
+
+	    if($argv[1] < -67768036191676799 || $argv[1] > 67768036191676799)
+	    {
+                privmsg($src->{svr}, $src->{chan}, trans('Invalid 64-Bit Timestamp. Must be between minus 67768036191676799 and positive 67768036191676799').q{.});
+                return;
+	    }
+
+	    if(Scalar::Util::looks_like_number($argv[1])) {
+		    my $output;
+		    my $timevariable;
+		    $timevariable = "date -d @".$argv[1];
+		    $output = `$timevariable`;
+		    $output =~ s/\n//g;
+		    privmsg($src->{svr}, $src->{chan}, "Unixtime format: \2$argv[1]\2 is \2$output\2");
+	    } else {
+		    privmsg($src->{svr}, $src->{chan}, "ERROR: Incorrect time format.");
+	    }
+    }
+    elsif(lc($argv[0]) eq "to")
+    {
+	    if(lc($argv[1]) eq "now")
+	    {
+		    privmsg($src->{svr}, $src->{chan}, "Datetime format: \2NOW\2 is \2".time()."\2");
+		    return;
+	    }
+
+        my $msg;
+	    for(my $i = 1; $i < scalar(@argv); $i++) {
+		    $msg .= " ".$argv[$i];
+	    }
+
+        if(str2time($msg))
+        {
+            privmsg($src->{svr}, $src->{chan}, "Datetime format:\2$msg\2 is \2".str2time($msg)."\2");
+        }
+        else
+        {
+            privmsg($src->{svr}, $src->{chan}, "ERROR: Incorrect or Unknown time format.");
+        }
+    }
+    else
+    {
+        privmsg($src->{svr}, $src->{chan}, trans('Syntax: unixtime [to/from] [dateformat/unixtime]').q{.});
+    }
     return 1;
 }
 
 # Start initialization.
-API::Std::mod_init('UnixTime', 'Peter', '1.00', '3.0.0a11');
-# build: perl=5.010000
+API::Std::mod_init('UnixTime', 'Russell M Bradford', '2.00', '3.0.0a11');
+# build: cpan=Date::Parse perl=5.010000
 
 __END__
 
@@ -92,12 +116,15 @@ __END__
 
 =head1 VERSION
 
- 1.00
+ 2.00
 
 =head1 SYNOPSIS
 
- <+[nas]peter> !unixtime 111111
-  Unixtime format: 111111 is Fri Jan  2 06:51:51 UTC 1970
+ <SomeUser> !unixtime from 111111
+ <SomeBot> Unixtime format: 111111 is Fri Jan  2 06:51:51 UTC 1970
+
+ <SomeUser> !unixtime to Fri Jan  2 06:51:51 UTC 1970
+ <SomeBot> Datetime format: Fri Jan  2 06:51:51 UTC 1970 is 111111 
 
 =head1 DESCRIPTION
 
@@ -105,11 +132,11 @@ This command will change a unixtime format to normal readable time.
 
 =head1 AUTHOR
 
-This module was written by Peter.
+This module was written by Russell M Bradford.
 
 =head1 LICENSE AND COPYRIGHT
 
-This module is Copyright 2012-2014 RedStone Development Group. All rights
+This module is Copyright 2014 Russell M Bradford. All rights
 reserved.
 
 This module is released under the same licensing terms as RedStone itself.
@@ -117,3 +144,4 @@ This module is released under the same licensing terms as RedStone itself.
 =cut
 
 # vim: set ai et ts=4 sw=4:
+
